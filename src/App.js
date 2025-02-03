eceb7a25198044c548ab641561e3f121b3f8a296
MarcoMondelli91 <info@marcomondelli.net>
2025-02-03 19:36:12 +0100

    5. Styled Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+//Global Style
+import GlobalStyle from "./components/GlobalStyle";
 //import pages
 import AboutUS from "./pages/AboutUS";
 
 function App() {
   return (
     <div className="App">
+        <GlobalStyle />
         <AboutUS />
     </div>
   );

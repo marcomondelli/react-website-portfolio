0fbb7d4e4deaf59aaaded1d306ef3a75f9a17c57
MarcoMondelli91 <info@marcomondelli.net>
2025-02-03 19:55:16 +0100

    6. Styled Services

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -19,6 +19,7 @@ button{
     background: transparent;
     color: white;
     transition: all 0.5s ease;
+    font-family: 'Inter', sans-serif;
     &:hover{
         background-color: #23d997;
         color: white;

f824f61b0fab7414b8e5d9150deb52695232c4a2
MarcoMondelli91 <info@marcomondelli.net>
2025-02-03 20:13:53 +0100

    8. Nav Creation

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 //import pages
 import AboutUS from "./pages/AboutUS";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
         <GlobalStyle />
+        <Nav />
         <AboutUS />
     </div>
   );

49859d8edd133d369c741d50ddf26e1b12aa1ebd
MarcoMondelli91 <info@marcomondelli.net>
2025-02-03 19:09:38 +0100

    3. Services Section

--- src/pages/AboutUS.js
+++ src/pages/AboutUS.js
@@ -1,9 +1,15 @@
 import React from "react";
 //page components
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 const AboutUS = () => {
-    return <AboutSection />;
+    return (
+        <>
+            <AboutSection />
+            <ServicesSection />
+        </>
+    )
 };
 
 export default AboutUS;
